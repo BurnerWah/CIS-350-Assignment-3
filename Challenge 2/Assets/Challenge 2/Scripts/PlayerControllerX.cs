a937b83ff5168dc19923e0ae526693450f012dc3
Jaden <jpleasants@mail.bradley.edu>
2022-02-05 20:30:04 -0800

    Base stuff for challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    public GameObject dogPrefab;
+
+    private bool canSpawnDog = true;
+    void Start() {
+        canSpawnDog = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // On spacebar press, send dog
+        if (canSpawnDog && Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawnDog = false;
+            StartCoroutine(UnlockSpawning());
+        }
+    }
+
+    private IEnumerator UnlockSpawning() {
+        yield return new WaitForSeconds(2.5f);
+        canSpawnDog = true;
+    }
+}
